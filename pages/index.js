bfbd2aa6a3a1c4d0fade32723f7ef236639480ea
Marcio Soares <biodevs@icloud.com>
2021-11-24 12:47:07 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
   return (
-    <h1>HOME</h1>
+    <>
+      <h1>HOME</h1>
+      <Link href='/sobre'>
+        <a>Acessa a paágina Sobre</a>
+      </Link>
+    </>
   )
 }
 
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Sobre() {
+  return (
+    <>
+      <h1>Sobre</h1>
+      <Link href='/'>
+        <a>Acessa a paágina Home</a>
+      </Link>
+    </>
+  )
+}
+
+export default Sobre;
